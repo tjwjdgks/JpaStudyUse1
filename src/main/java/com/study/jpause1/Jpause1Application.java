03e824be4bf14e1450f5c6464d69f8cff44c72b2
tjwjdgks <tjwjdgks43@gmail.com>
2021-12-20 14:00:12 +0900

    "init"

---
+++ src/main/java/com/study/jpause1/Jpause1Application.java
@@ -0,0 +1,13 @@
+package com.study.jpause1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpause1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpause1Application.class, args);
+    }
+
+}
